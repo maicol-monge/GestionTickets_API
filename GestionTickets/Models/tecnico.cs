e981eae2bdd801c8c1b0c6abf420cb4a01d62750
Paola Vidal <paola.vidal@catolica.edu.sv>
2025-05-24 16:24:51 -0600

    Parte finalizada de gestión de usuarios

--- GestionTickets/Models/tecnico.cs
+++ GestionTickets/Models/tecnico.cs
@@ -7,7 +7,7 @@ namespace GestionTickets.Models
         [Key]
         public int id_tecnico { get; set; }
         public int id_usuario { get; set; }
-        public int id_usuarioid_categoria { get; set; }
+        public int id_categoria { get; set; }
 
     }
 }
